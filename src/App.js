fc8aceaebec144691b30d7dfe6d18a38ed6ec55c
unknown <sunil.raipur@gmail.com>
2021-11-13 18:12:11 +0530

    created a react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  //alert("")
+
+  console.log("This is the part of console written above return")
+
+  return (
+
+    <div className="App">
+      Welcome ji
+    </div>
+    
+  );
+}
+
+export default App;
