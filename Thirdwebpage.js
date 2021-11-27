22455db43421ad4f9c4675ece161aa6bbf21b6a8
soni prajapati <soni963171@gmail.com>
2021-11-27 17:05:09 +0530

    webpage

---
+++ Thirdwebpage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "../component/components/Navigation";
+
+
+const Thirdwebpage=()=>{
+    return(
+        <>
+        <Navigation/>
+        </>
+    )
+}
+export default Thirdwebpage;
