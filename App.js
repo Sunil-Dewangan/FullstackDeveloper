a09ca6a37bc6f1d94129b0f30a8a99f2e830cd47
Chandan12799 <93399343+Chandan12799@users.noreply.github.com>
2021-12-14 18:31:07 +0530

    Asingment

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react"
 import './App.css';
 //import { HashRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./SecondUiAssessment/Homepage";
+//import Homepage from "./SecondUiAssessment/Homepage";
 //import Webpage from "./Assignment/Webpage";
 //import Navigation from "./Assignment/Newassingnment/Navigation";
 //import Firstpage from "./Assignment/Newassingnment/Firstpage";
-
+import Ashamaa from "./new_assignment/Ashamaa"
 
 function App() {
 
@@ -13,8 +13,9 @@ function App() {
 
     <>
       
-        {/* <Navigation/> */}
-        <Homepage />
+        {/* <Navigation/>
+        <Homepage /> */}
+        <Ashamaa/>
         {/*<Routes>
             <Route exact path="/" element={<Webpage/>}/>
             <Route exact path="/Secondwebpage" element={<Secondwebpage/>}/>
